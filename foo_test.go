996755e35c37de43a69b06dbc3ecbc05af07aeb9
Alexander Trakhimenok <alex@trakhimenok.com>
2025-12-28 23:33:28 +0000

    fix(ci): remove unused dependencies and replace assert with inline nil check in tests

--- foo_test.go
+++ foo_test.go
@@ -1,12 +1,12 @@
 package go_ci_action
 
 import (
-	// we add the 'assert' package to the imports to have a go.sum file for caching in CI
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func Test_bar(t *testing.T) {
-	v := bar()
-	assert.Nil(t, v)
+
+	if v := bar(); v != nil {
+		t.Errorf("bar() should return nil, got %v", v)
+	}
 }
